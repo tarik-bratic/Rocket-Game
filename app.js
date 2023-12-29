c2ce3154a420b5867f62c137ac0222c612b50635
Tarik Bratic <tarik.bratic@gmx.com>
2023-12-30 00:14:40 +0100

    keypress added

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 let destn_value = 250;
 let velocity_value = -25;
 
-function updateGame() {
+function updateGame(pressure) {
 
   // user input
-  let pressure = document.getElementById('gameInput').value;
+  // let pressure = document.getElementById('gameInput').value;
 
-  // Error message
-  if (pressure < 0 || pressure > 100) {
-    alert("Invalid pressure, 0 to 100");
-    return;
-  }
+  // // Error message
+  // if (pressure < 0 || pressure > 100) {
+  //   alert("Invalid pressure, 0 to 100");
+  //   return;
+  // }
 
   //calculate destn and velocity
   destn_value = destn_value + velocity_value + (0.1 * pressure - 1.5) / 2;
@@ -25,12 +25,26 @@ function updateGame() {
 
     destn_value = 250;
     velocity_value = -25;
+    pressure = 25;
   }
 
   // display on the screen
   document.getElementById('distance').innerHTML = destn_value;
-  document.getElementById('velocity').innerHTML = velocity_value;
+  document.getElementById('velocity').innerHTML = Math.trunc(velocity_value);
 
 }
 
-document.getElementById('gameBtn').addEventListener("click", updateGame);
+function getPressure(currPressure) {
+  return currPressure + 1;
+}
+
+let pressure = 25;
+
+// document.getElementById('gameBtn').addEventListener("click", updateGame);
+addEventListener("keydown", (event) => {
+  if(event.code == "KeyW") {
+    updateGame(pressure);
+    pressure = getPressure(pressure);
+    console.log(event.code);
+  }
+})
