ff04d08336ab6cb1b92ff98e9406dbd176ee2859
Tarik Bratic <tarik.bratic@gmx.com>
2024-01-17 20:52:42 +0100

    fix bug

--- app.js
+++ app.js
@@ -2,123 +2,68 @@
 let destn_value = 250;
 let velocity_value = -25;
 let pressure = 25;
-let notPress = true;
+let noContact = true;
 
-
+const ship = document.querySelector("#rs");
 
 /* The game is ongoing (find other way) */
 setInterval(updateGameM, 1000);
 
-/**
- * The function is about updating the game and check conditions and what not.
- * DESKTOP
- */
 function updateGameM() {
 
-  const rs = document.querySelector("#rs");
-
-  console.log(pressure)
-
-  /* Event listeners for when key "W" is press or not */
-  rs.addEventListener("touchmove", (event) => {
-    console.log("press")
-    pressure += 1;
-    if (pressure > 100) pressure = 100;
-    if (pressure < 0) pressure = 0;
-    // notPress = false;
+  //When the finger is moving around, increase pressure +1
+  ship.addEventListener("touchmove", (event) => {
+    noContact = false;
+    pressure++;
+    if (pressure > 100) {
+      pressure = 100;
+    }
   });
 
-  rs.addEventListener("touchend", (event) => {
-    pressure =- 1;
-    if (pressure < 0) pressure = 0;
-    console.log("Pointer moved out");
-  });
+  ship.addEventListener("touchend", (event) => {
+    noContact = true;
+  })
 
-  /* If no key is pressed decrease pressure */
-  // if (notPress) {
-  //   pressure =- 1;
-  //   if (pressure < 0) pressure = 0;
-  // }
+  if (noContact) {
+    pressure = decreasePressure(pressure);
+  }
 
-  /* Calculate the new values based of the amount of pressure */
+  /* Calculate new values */
   destn_value = destn_value + velocity_value + (0.1 * pressure - 1.5) / 2;
   velocity_value = velocity_value + (0.1 * pressure - 1.5);
 
-  /* Rules of the game (conditions) */
-  if (destn_value <= 0) {
-    if (velocity_value < -2) {
-      alert("You lost");
-    } else {
-      alert("You won");
-    }
-
-    destn_value = 250;
-    velocity_value = -25;
-    pressure = 25;
-  }
+  checkGameCondition(destn_value, velocity_value);
 
   /* Update index.html with new values */
   document.getElementById('distance').innerHTML = Math.trunc(destn_value);
   document.getElementById('velocity').innerHTML = Math.trunc(velocity_value);
+  document.getElementById('pressure').innerHTML = Math.trunc(pressure);
 
 }
 
-/**
- * The function is about updating the game and check conditions and what not.
- * DESKTOP
- */
-function updateGameD() {
-
-  console.log(pressure)
-
-  /* Event listeners for when key "W" is press or not */
-  addEventListener("keydown", (event) => {
-    if (event.code == "KeyW") {
-      pressure += 1;
-      if (pressure > 100) pressure = 100;
-      if (pressure < 0) pressure = 0;
-      notPress = false;
-    }
-  });
-
-  addEventListener("keyup", (event) => {
-    notPress = true;
-  })
-
-  /* If no key is pressed decrease pressure */
-  if (notPress) {
-    pressure =- 1;
-    if (pressure < 0) pressure = 0;
+function decreasePressure(pressure) {
+  pressure -= 1;
+  if (pressure < 0) {
+    pressure = 0;
   }
 
-  /* Calculate the new values based of the amount of pressure */
-  destn_value = destn_value + velocity_value + (0.1 * pressure - 1.5) / 2;
-  velocity_value = velocity_value + (0.1 * pressure - 1.5);
+  return pressure;
+}
 
-  /* Rules of the game (conditions) */
-  if (destn_value <= 0) {
-    if (velocity_value < -2) {
+/* Rules of the game (conditions) */
+function checkGameCondition(destn, velo) {
+  if (destn <= 0) {
+    if (velo < -2) {
       alert("You lost");
     } else {
       alert("You won");
     }
-
-    destn_value = 250;
-    velocity_value = -25;
-    pressure = 25;
+    resetGame();
   }
-
-  /* Update index.html with new values */
-  document.getElementById('distance').innerHTML = destn_value;
-  document.getElementById('velocity').innerHTML = Math.trunc(velocity_value);
-
 }
 
-/**
- * 
- * @param {*} flag if key is pressed true if not false
- * @returns 
- */
-function keyPress(flag = false) {
-  return flag;
+function resetGame() {
+  destn_value = 250;
+  velocity_value = -25;
+  pressure = 25;
 }
